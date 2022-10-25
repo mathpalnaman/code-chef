5d142e2d02e2645fe873fe821e4ec295d6281d83
mathpalnaman <112200396+mathpalnaman@users.noreply.github.com>
2022-10-26 00:00:31 +0530

    Create remainder.cpp

---
+++ remainder.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int t;
+     cin>>t;
+     for(int i=0;i<t;i++)
+         {
+             int a,b;
+             cin>>a>>b;
+             int p=a%b;
+         
+             cout<<p<<endl;
+         }
+         
+          return 0;
+}
