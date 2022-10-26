8c7ff899c5bd1255a2ee50aad5076bf36db63c12
mathpalnaman <112200396+mathpalnaman@users.noreply.github.com>
2022-10-26 21:47:46 +0530

    Create reach on time.cpp

---
+++ reach on time.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,n;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{cin>>n;
+	    if(n>=30)
+	    {cout<<"YES"<<endl;}
+	    else
+	   { cout<<"NO"<<endl;}
+	}
+	return 0;
+}
