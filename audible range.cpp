f5cdb85d9ce71fdd601939fcf56001f0e46c396f
mathpalnaman <112200396+mathpalnaman@users.noreply.github.com>
2022-10-26 20:33:31 +0530

    Create audible range.cpp

---
+++ audible range.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,n;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{cin>>n;
+	    if(n>=67 && n<=45000)
+	    {cout<<"YES"<<endl;}
+	    else
+	    {cout<<"NO"<<endl;}
+	}
+	
+	return 0;
+}
