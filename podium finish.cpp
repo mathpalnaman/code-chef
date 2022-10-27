df0d94c602735ed826027d23c43fa9dc64a3cd13
mathpalnaman <112200396+mathpalnaman@users.noreply.github.com>
2022-10-28 01:46:30 +0530

    Create podium finish.cpp

---
+++ podium finish.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	int a,b,n;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	    cin>>a>>b;
+	    n=a+b;
+	    cout<<n<<endl;
+	}
+	return 0;
+}
