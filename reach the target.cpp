bb13c795a5cc162f8f2bfb2ccf766bd7585675f4
mathpalnaman <112200396+mathpalnaman@users.noreply.github.com>
2022-10-27 19:49:13 +0530

    Create reach the target.cpp

---
+++ reach the target.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,n,a,b;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{int a,b;
+	 cin>>a>>b;
+	 n=a-b;
+	 cout<<n<<endl;
+     	}
+	return 0;
+}
