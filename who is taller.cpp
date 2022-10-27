49bd8f041eaeec1bfea775ab109028bab28daf5b
mathpalnaman <112200396+mathpalnaman@users.noreply.github.com>
2022-10-27 16:35:58 +0530

    Create who is taller.cpp

---
+++ who is taller.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int t,x,y;
+   cin>>t;
+   for(int i=0;i<t;i++)
+   {
+       cin>>x>>y;
+       if(x>y)
+       cout<<'A'<<endl;
+       else
+       cout<<'B'<<endl;
+   }
+	return 0;
+}
