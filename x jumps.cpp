e0184cfb94293b81edeae7d269bbf7432e8f897d
mathpalnaman <112200396+mathpalnaman@users.noreply.github.com>
2022-10-31 23:07:47 +0530

    Create x jumps.cpp

---
+++ x jumps.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int t,x,y;
+   cin>>t;
+   for(int i=0;i<t;i++)
+   {
+       cin>>x>>y;
+       
+/*	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	*/    cout<< x%y + x/y<<endl;
+	}
+	return 0;
+}
+
