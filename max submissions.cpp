1ace140cab5bea15d600e1aa30a032f7f1c51186
mathpalnaman <112200396+mathpalnaman@users.noreply.github.com>
2022-11-01 23:02:03 +0530

    Create max submissions.cpp

---
+++ max submissions.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t,n,x;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>x;
+        n=x*60;
+        cout<<n/30<<endl;
+    }
+   
+   
+	return 0;
+}
