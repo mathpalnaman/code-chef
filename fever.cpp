12695bc77e14a39590e3063d1c7fa6c098683bd0
mathpalnaman <112200396+mathpalnaman@users.noreply.github.com>
2022-11-14 22:48:06 +0530

    Create fever.cpp

---
+++ fever.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t,x;
+    cin>>t;
+    while(t--)
+    {cin>>x;
+        if(x>98) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
